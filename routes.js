0b76b9180b8072b026f14ef7b54e42fae36a0799
pyetro-cod <pyetro.f@aluno.uepb.edu.br>
2026-05-19 23:24:18 -0300

    rota de contato

--- routes.js
+++ routes.js
@@ -3,7 +3,9 @@ const route = express.Router();
 
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
 
+const {loginRequired} = require('./src/middlewares/middleware');
 
 // Rotas da home
 route.get('/', homeController.index);
@@ -11,9 +13,14 @@ route.get('/', homeController.index);
 
 //login
 route.get('/login/index',loginController.index);
-route.post('/login/register',loginController.register)
-route.post('/login/login',loginController.login)
-route.get('/login/logout',loginController.logout)
+route.post('/login/register',loginController.register);
+route.post('/login/login',loginController.login);
+route.get('/login/logout',loginController.logout);
+
+
+//Rodas de Contato
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
 
 
 
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -10,12 +10,24 @@ exports.outroMiddleware = (req, res, next) => {
 };
 
 exports.checkCsrfError = (err, req, res, next) => {
-  if(err && 'EBADCSRFTOKEN' === err.code) {
+  if(err) {
     return res.render('404');
   }
+
+  next();
 };
 
 exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
   next();
 };
+
+exports.loginRequired = (req,res,next)  =>{
+  if(!req.session.user){
+    req.flash('errors', 'Você precisa fazer o login.');
+    req.session.save(() => res.redirect('/'));
+    return
+  }
+
+  next();
+}
